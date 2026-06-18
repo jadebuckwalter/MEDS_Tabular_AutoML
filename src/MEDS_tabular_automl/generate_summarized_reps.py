fbd978192a34158317fa6f2a7d617696996416ae
Jade Buckwalter <jbuckwal@mit.edu>
2026-06-18 12:59:11 -0400

    Aggregation function edits

--- src/MEDS_tabular_automl/generate_summarized_reps.py
+++ src/MEDS_tabular_automl/generate_summarized_reps.py
@@ -44,9 +44,11 @@ def sparse_aggregate(sparse_matrix: sparray, agg: str) -> np.ndarray | coo_array
         csc.sort_indices()
         num_cols = csc.shape
         last_values = np.zeros(num_cols, dtype=csc.dtype)
-        has_elements = np.diff(csc.indptr) > 0
-        last_data_indices = csc.indptr[1:][has_elements] - 1
-        last_values[has_elements] = csc.data[last_data_indices]
+        col_counts = np.diff(csc.indptr)
+        active_cols = np.where(col_counts > 0)
+        if len(active_cols) > 0:
+            last_data_indices = csc.indptr[active_cols + 1] - 1
+            last_values[active_cols] = csc.data[last_data_indices]
         merged_matrix = last_values
     else:
         raise ValueError(f"Aggregation method '{agg}' not implemented.")
